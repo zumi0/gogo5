c1982d3158cbe8e8af1211818110def4ea874a9f
Yuto NAKAIZUMI <yutonakaizumi@YutonoMacBook-Pro.local>
2019-07-02 11:25:57 +0900

    add

---
+++ test_sqrt.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "math"
+)
+
+func main() {
+  fmt.Printf("Now you have %g problerms.", math.Sqrt(4))
+  fmt.Println(math.Pi)
+}
---
+++ test_zero.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+  var (
+    i int
+    f float64
+    b bool
+    s string
+  )
+  fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
